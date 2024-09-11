6635e0a8f89923849732a805a6d2452f8a720563
José Magalhães <joses.magalhaes@hotmail.com>
2024-09-11 12:09:55 -0300

    :construction: Creating game objects based on sprites

--- main.py
+++ main.py
@@ -34,13 +34,26 @@ vel_x_missil = 0
 pos_x_missil = 200
 pos_y_missil = 300
 
+triggered = False
+
 
 rodando = True
+
+player_rect = playerImg.get_rect()
+alien_rect = alien.get_rect()
+missil_rect = missil.get_rect()
 
 def respawn():
     x = 1350
     y = random.randint(1,640)
     return [x, y]
+
+def respawn_missil():
+    triggered = False
+    respawn_missil_x = pos_player_x
+    respawn_missil_y = pos_player_y
+    vel_x_missil = 0
+    return [respawn_missil_x, respawn_missil_y, triggered, vel_x_missil]
 
 
 while rodando:
@@ -58,15 +71,43 @@ while rodando:
     tecla = pygame.key.get_pressed()
     if (tecla[pygame.K_UP] or tecla[pygame.K_w]) and pos_player_y > 1:
         pos_player_y -=1
+
+        if not triggered:
+            pos_y_missil -=1 
+
     if (tecla[pygame.K_DOWN] or tecla[pygame.K_s]) and pos_player_y < 665:
-        pos_player_y +=1    
+        pos_player_y +=1
+
+        if not triggered:
+            pos_y_missil +=1 
+
+    if tecla[pygame.K_SPACE]:
+        triggered = True
+        vel_x_missil = 2  
     
     if pos_alien_x == 50:
         pos_alien_x = respawn()[0]
         pos_alien_y = respawn()[1]
 
+    if pos_x_missil == 1300:
+        pos_x_missil, pos_y_missil, triggered, vel_x_missil = respawn_missil()
+
+    player_rect.y = pos_player_y
+    player_rect.x = pos_player_x
+
+    missil_rect.x = pos_x_missil
+    missil_rect.y = pos_y_missil
+
+    alien_rect.x = pos_alien_x
+    alien_rect.y = pos_alien_y
+
     x-=2
     pos_alien_x -=1
+    pos_x_missil += vel_x_missil
+
+    pygame.draw.rect(screen, (255, 0, 0), player_rect, 4)
+    pygame.draw.rect(screen, (255, 0, 0), missil_rect, 4)
+    pygame.draw.rect(screen, (255, 0, 0), alien_rect, 4)
 
     screen.blit(alien, (pos_alien_x, pos_alien_y))
     screen.blit(missil, (pos_x_missil, pos_y_missil))
